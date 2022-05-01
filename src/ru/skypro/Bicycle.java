2d5ce4ab8f94cc57f4a2059e564b066b1413fa9b
AleksTMN <kirilloff_77@mail.ru>
2022-05-01 21:21:00 +0500

    1

--- src/ru/skypro/Bicycle.java
+++ src/ru/skypro/Bicycle.java
@@ -4,4 +4,13 @@ public class Bicycle extends Transport {
     public Bicycle(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
+
+    public void check(Bicycle bicycle) {
+        if (bicycle != null) {
+            System.out.println("Обслуживаем " + bicycle.modelName);
+            for (int i = 0; i < bicycle.wheelsCount; i++) {
+                updateTyre();
+            }
+        }
+    }
 }
